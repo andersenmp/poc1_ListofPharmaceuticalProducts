56db14dc9fa2743a6c3f6441992a16d3827d4b3a
Andersen Pecorone <andersen.pecorone@atenta.eu>
2018-03-15 14:42:15 +0100

    Change to SQLite

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,13 +6,23 @@
 <div class="col-sm-4">
     <h4>Requirments</h4>
     <pre>
-        "php": ">=7.1.3",
-        "doctrine/dbal": "^2.6",
-        "fideloper/proxy": "~4.0",
-        "jasig/phpcas": "1.3.x-dev",
-        "laravel/framework": "5.6.*",
-        "laravel/tinker": "~1.0",
-        "subfission/cas": "dev-master"
+        "require": {
+            "php": ">=7.1.3",
+            "doctrine/dbal": "^2.6",
+            "fideloper/proxy": "~4.0",
+            "jasig/phpcas": "1.3.x-dev",
+            "laravel/framework": "5.6.*",
+            "laravel/tinker": "~1.0",
+            "subfission/cas": "dev-master"
+        },
+        "require-dev": {
+            "filp/whoops": "~2.0",
+            "nunomaduro/collision": "~1.1",
+            "fzaninotto/faker": "~1.4",
+            "mockery/mockery": "~1.0",
+            "phpunit/phpunit": "~7.0",
+            "symfony/thanks": "^1.0"
+        },
     </pre>
 </div>
 
